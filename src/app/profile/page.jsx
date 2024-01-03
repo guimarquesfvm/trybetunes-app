160d18449e3623899eb800b317d20149a5b02d3d
Guilherme Marques <guimarquesjob@gmail.com>
2024-01-03 14:50:30 -0300

    feat: profile page css responsivity adjusts

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,29 +5,37 @@ function Profile() {
     <>
       <div className="flex items-center justify-center flex-1 max-md:flex-col"></div>
       <div className="h-2/3 w-full bg-white overflow-visible flex justify-center">
-        <div className="flex text-[#3D495C]">
-          <img src="" alt="" className=" rounded-full size-60 relative bottom-28 shadow-lg shadow-slate-700" />
+        <div className="flex flex-col max-w-[90%] lg:flex-row text-[#3D495C]">
+          <div className="absolute max-xl:top-4 xl:relative xl:bottom-28 flex xl:flex-col items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
+              alt=""
+              className=" rounded-full size-60 shadow-lg shadow-slate-700 bg-white"
+            />
+          </div>
 
-          <div className="flex-1 ml-9 max-w-lg flex flex-col gap-4 mt-4 items-start">
+          <div className="flex-1 lg:ml-9 max-w-lg flex flex-col gap-4 mt-4 items-start">
             <div>
               <h2 className="text-2xl font-bold">Name</h2>
-              <p>Username</p>
+              <p>Your name</p>
             </div>
             <div>
               <h2 className="text-2xl font-bold">Email</h2>
-              <p>foo@bar.com</p>
+              <p>Your Email</p>
             </div>
             <div>
               <h2 className="text-2xl font-bold">Description</h2>
               <p>
-                {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                est lorem, fermentum nec hendrerit quis, rhoncus sed massa. Duis
-                varius bibendum efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                est lorem, fermentum nec hendrerit quis, rhoncus sed massa. Duis
-                varius bibendum efficitur.`}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis officiis delectus commodi eveniet sequi ea. Sint
+                quidem temporibus dolor veniam! Similique repellat voluptatibus
+                accusantium placeat corporis exercitationem optio doloribus
+                excepturi?
               </p>
             </div>
-            <button className="rounded-full bg-[var(--btnblue)] px-6 py-3 text-white font-bold">EDIT PROFILE</button>
+            <button className="rounded-full bg-[var(--btnblue)] px-6 py-3 text-white font-bold">
+              EDIT PROFILE
+            </button>
           </div>
         </div>
       </div>
